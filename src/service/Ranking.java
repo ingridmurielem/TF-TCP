c296785847f93f0b233eaffbfd2f666735b8533e
ingrid <murielem.ingrid@gmail.com>
2018-06-01 10:52:01 -0300

    Player adicionado em model e Ranking em service

---
+++ src/service/Ranking.java
@@ -0,0 +1,15 @@
+package service;
+
+import model.Player;
+import java.util.List;
+
+public class Ranking {
+
+    List<Player> playerScore;
+    List<Player> ranking;
+    
+    public List <Player> getRanking() {
+        return ranking;
+    }
+
+}
